8bd5ec18ace9d5059e9a9face0e81b938541daab
Kykli <luukkanen.eetu@gmail.com>
2018-05-24 13:12:27 +0300

    Graph work in progress

--- index.php
+++ index.php
@@ -7,13 +7,18 @@
         </form>
 
         <?php
-            if (isset($_REQUEST["outside"])){
+            /*if (isset($_REQUEST["outside"])){
                 echo shell_exec("python3 /var/www/html/outside.py");
             }
             
             if (isset($_REQUEST["inside"])){
                 echo shell_exec("python3 /var/www/html/inside.py");
-            }
+            }*/
+            header("refresh:10;");
+            echo("Outside temperature is: ");
+            echo shell_exec("python3 /var/www/html/outside.py");
+            echo("<br>"."Inside temperature is: ");
+            echo shell_exec("python3 /var/www/html/inside.py");
         ?>
 
     </body>
--- logger.py
+++ logger.py
@@ -1,11 +1,33 @@
 from time import sleep, strftime, time
+import matplotlib.pyplot as plt
 import os
+import csv
+
+x = []
+y = []
+
+def sensor1():
+    for i in os.listdir("/sys/bus/w1/devices"):
+        if i == "28-01183085f3ff":
+            outsidesensor = i
+    return outsidesensor
 
 def sensor2():
     for j in os.listdir("/sys/bus/w1/devices"):
         if j == "28-01183087f8ff":
             outsidesensor = j
     return outsidesensor
+
+def read(insidesensor):
+    location = "/sys/bus/w1/devices/" + insidesensor + "/w1_slave"
+    file = open(location)
+    text = file.read()
+    file.close()
+    temperatureline = text.split("\n")[1]
+    temperaturedata = temperatureline.split(" ")[9]
+    temperature = float(temperaturedata[2:])
+    celsius = temperature / 1000
+    return celsius
 
 def read(outsidesensor):
     location = "/sys/bus/w1/devices/" + outsidesensor + "/w1_slave"
@@ -21,9 +43,31 @@ def read(outsidesensor):
 def outsideTemperature(outsidesensor):
     with open ("outside_temp.csv", "a") as logging:
         temp = read(outsidesensor)
-        logging.write("{0},{1}\n".format(strftime("%Y-%m-%d %H:%M:%S"),str(temp)))
+        logging.write("{0},{1}\n".format(strftime("%Y-%m-%d %H:%M:%S"),str("%0.1f" % temp)))
+
+def insideTemperature(insidesensor):
+    with open ("inside_temp.csv", "a") as logging:
+        temp = read(insidesensor)
+        logging.write("{0},{1}\n".format(strftime("%Y-%m-%d %H:%M:%S"),str("%0.1f" % temp)))
+
+# Work in progress
+def graph():
+    with open ("outside_temp.csv", "r") as outcsvfile:
+        plots = csv.reader(outcsvfile, delimiter=",")
+        for row in plots:
+            x.append(str(row[0]))
+            y.append(float(row[1]))
+    
+    plt.plot(x,y)
+    plt.xlabel("x")
+    plt.ylabel("y")
+    #plt.title("")
+    plt.legend()
+    plt.savefig("temp.png")
 
 if __name__ == '__main__':
+    s1 = sensor1()
     s2 = sensor2()
+    insideTemperature(s1)
     outsideTemperature(s2)
-    
+    graph()
